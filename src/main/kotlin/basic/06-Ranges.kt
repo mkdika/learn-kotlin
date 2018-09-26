9d28fefc25a2be271a7b8d96c396e6d5b4f4a0d7
Maikel Chandika <mkdika@gmail.com>
2018-09-26 13:57:01 +0700

    refine ranges example.

--- src/main/kotlin/basic/06-Ranges.kt
+++ src/main/kotlin/basic/06-Ranges.kt
@@ -20,4 +20,29 @@ fun main(args: Array<String>) {
     println("countingDown: $countingDown")
     println("rangeTo: $rangeTo")
 
+    // loop range down to
+    println()
+    for (i in 50 downTo 1 step 2) {
+        print("$i ")
+    }
+
+    // loop range ascending to
+    println()
+    for (i in 50..100 step 2) {
+        print("$i ")
+    }
+
+    // demo range pattern matching
+    println("\n")
+    println(recognize('3'))
+    println(recognize('g'))
+    println(recognize('X'))
+    println(recognize('#'))
+}
+
+// range pattern matching with when
+fun recognize(c: Char) = when(c) {
+    in '0'..'9' -> "$c is a digit!"
+    in 'a'..'z', in 'A'..'Z' -> "$c is a letter!"
+    else -> "I don't know..maybe special char!"
 }
